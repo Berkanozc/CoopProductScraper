484c68df5f3e058bee7c98b18a737df6139787ba
ozcakib <berkan.ozcakir@hva.nl>
2021-05-04 23:58:47 +0200

    initial commit

---
+++ src/main/java/com/berkan/coopscraper/CoopScraperApplication.java
@@ -0,0 +1,13 @@
+package com.berkan.coopscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkan/coopscraper/CoopScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkan.coopscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
